b2f7795875ba564408568d93e5c8bc480e10a3b4
Dhruvit1371 <dhruvit09876543@gmail.com>
2025-05-25 10:35:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,11 +68,11 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Dhruvitmovie:Dhruvitmovie@cluster0.zcde1qd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Dhruvitnew:Dhruvit@dhruvitnew.yhidpd6.mongodb.net/?retryWrites=true&w=majority&appName=Dhruvitnew")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "Dhruvitnew")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Dhruvitnew')
 
-MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', True)) # Set True or False
+MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
 # If Multiple Database Is True Then Fill All Three Below Database Uri Else You Will Get Error.
 O_DB_URI = environ.get('O_DB_URI', "")   # This Db Is For Other Data Store
